5713dc3b65f02b4e031946c2fe27a6a779a635e4
Yaroslav Solonnikov <solonnikov@iMac-Yaroslav.local>
2018-01-31 03:06:42 +0200

    added config to env.prod and fixed build -prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  accounts: {
+    getPolicies: 'https://accounts.gigya.com/accounts.getPolicies',
+    setPolicies: 'https://accounts.gigya.com/accounts.setPolicies',
+  },
+  credentials: {
+    userKey: 'AJA3Cw9XcJZf',
+    secret: '1J%2BYxAY47khnuXf4GKSggLpPFBbQv8Hq',
+    apiKey: '3_inujb44QPskKBok5VwhYnqy40eaVrwAJXXLsqaHRI_6DCM3KHhxNXjjcFQe0PASK',
+  }
 };
