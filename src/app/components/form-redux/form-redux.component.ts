76938be2ff37bd9f50be0ea13b4d699db5e47bc5
Yaroslav Solonnikov <solonnikov@iMac-Yaroslav.local>
2018-02-01 23:33:26 +0200

    added template driven form with working FORM_CHANGED action, reducer and store (locally)

--- src/app/components/form-redux/form-redux.component.ts
+++ src/app/components/form-redux/form-redux.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 // Redux imports
 import { NgRedux } from '@angular-redux/store';
 import { IAppState } from '../../store';
 import { AccountOptions } from '../../models/AccountOptions';
-import { GET, UPDATE } from '../../actions';
+import { GET, FORM_CHANGED } from '../../actions';
 import { select } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
 
@@ -15,58 +16,30 @@ import { Observable } from 'rxjs/Observable';
   styleUrls: ['./form-redux.component.css']
 })
 export class FormReduxComponent implements OnInit, OnChanges {
-  @Input() formDefault: AccountOptions;
-  editDataForm: FormGroup;
+  editDataForm: Observable<AccountOptions>;
 
-  @Output() dataEdited = new EventEmitter<AccountOptions>();
+  accountOptions: AccountOptions;
 
   constructor(
     private ngRedux: NgRedux<IAppState>,
-    public fb: FormBuilder
+    public router: Router
   ) {
-
-    this.createForm();
+    this.editDataForm = ngRedux.select('editDataForm');
   }
 
   ngOnInit() {
-    this.getData();
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    // Checking for changes and form validity
-    if (changes['formDefault'] && typeof this.formDefault === 'object') {
-      this.editDataForm.setValue(this.formDefault);
-    }
-  }
+  ngOnChanges() {
 
-  getData() {
-    this.ngRedux.dispatch({ type: GET });
-  }
-
-  createForm(): void {
-    const defaultLanguageRegex: any = '([a-zA-Z])+';
-    this.editDataForm = this.fb.group({
-      allowUnverifiedLogin: [{ value: false, disabled: true }],
-      defaultLanguage: ['en', Validators.compose([Validators.required, Validators.pattern(defaultLanguageRegex)])],
-      loginIdentifierConflict: ['ignore', Validators.required],
-      loginIdentifiers: ['email', Validators.required],
-      preventLoginIDHarvesting: [false, Validators.required],
-      sendAccountDeletedEmail: [false, Validators.required],
-      sendWelcomeEmail: [false, Validators.required],
-      verifyEmail: [false, Validators.required],
-      verifyProviderEmail: [false, Validators.required]
-    });
   }
 
   onEditSubmit() {
-    this.dataEdited.emit(this.editDataForm.value);
-    const value = this.editDataForm.getRawValue();
-    console.log(value);
     this.ngRedux.dispatch({
-      type: UPDATE, payload: {
-        path: 'editDataForm',
-        value: value
+      type: FORM_CHANGED, payload: {
+        value: this.editDataForm
       }
     });
+    this.router.navigate(['/readonly']);
   }
 }
