0b84d341a6b4749df0c94efcb3944062e54ae719
Yaroslav Solonnikov <solonnikov@iMac-Yaroslav.local>
2018-02-02 10:49:32 +0200

    added get session epic, have issues with action type etc

--- src/app/services/session-epics.ts
+++ src/app/services/session-epics.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ActionsObservable } from 'redux-observable';
-import { FORM_CHANGED, UPDATE_SUCCESS, UPDATE_ERROR } from '../actions';
-import { Observable } from 'rxjs/Observable';
+import { GET, FORM_CHANGED, UPDATE_SUCCESS, UPDATE_ERROR } from '../actions';
+import { Observable } from 'rxjs/Rx';
 import { IAppState } from '../store';
 
 import 'rxjs/add/observable/of';
@@ -23,8 +23,19 @@ export class SessionEpics {
   ) {
   }
 
-  update = (action$: ActionsObservable) => {
-    return action$.ofType(FORM_CHANGED)
+  get = (action) => {
+    return action.ofType(GET)
+      .mergeMap(() => {
+        return this.http.jsonp(`${this.accounts.getPolicies}?userkey=${this.credentials.userKey}&secret=${this.credentials.secret}&apikey=${this.credentials.apiKey}&format=jsonp&callback=JSONP_CALLBACK`, 'JSONP_CALLBACK')
+          .map(result => console.log(result))
+          .catch(error => Observable.of({
+            type: UPDATE_ERROR
+          }));
+      });
+  }
+
+  update = (action) => {
+    return action.ofType(FORM_CHANGED)
       .mergeMap(({ payload }) => {
         return this.http.jsonp(`${this.accounts.setPolicies}?userkey=${this.credentials.userKey}&secret=${this.credentials.secret}&apikey=${this.credentials.apiKey}&accountOptions=${payload}&format=jsonp&callback=JSONP_CALLBACK`, 'JSONP_CALLBACK')
           .map(result => ({
@@ -36,4 +47,5 @@ export class SessionEpics {
           }));
       });
   }
+
 }
