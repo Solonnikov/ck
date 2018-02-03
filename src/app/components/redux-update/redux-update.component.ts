22cb1a166e1765b68b610b7cbcef05ce5b4d70e6
Yaroslav Solonnikov <solonnikov@iMac-Yaroslav.local>
2018-02-03 18:11:34 +0200

    managed to update server data fropm redux component

--- src/app/components/redux-update/redux-update.component.ts
+++ src/app/components/redux-update/redux-update.component.ts
@@ -20,6 +20,17 @@ import { TypeModifier } from '@angular/compiler/src/output/output_ast';
 export class ReduxUpdateComponent implements OnInit {
   editDataForm: Observable<AccountOptions>;
 
+  accountOptions = {
+    allowUnverifiedLogin: null,
+    defaultLanguage: '',
+    loginIdentifierConflict: '',
+    loginIdentifiers: '',
+    preventLoginIDHarvesting: null,
+    sendAccountDeletedEmail: null,
+    sendWelcomeEmail: null,
+    verifyProviderEmail: null,
+  }
+
   constructor(
     private ngRedux: NgRedux<IAppState>,
     public router: Router
@@ -37,9 +48,16 @@ export class ReduxUpdateComponent implements OnInit {
     this.ngRedux.dispatch({
       type: FORM_CHANGED,
       payload: {
-        defaultLanguage: 'ua'
+        // allowUnverifiedLogin: this.accountOptions.allowUnverifiedLogin,
+        defaultLanguage: this.accountOptions.defaultLanguage,
+        // loginIdentifierConflict: this.accountOptions.loginIdentifierConflict,
+        // loginIdentifiers: this.accountOptions.loginIdentifiers,
+        // preventLoginIDHarvesting: this.accountOptions.preventLoginIDHarvesting,
+        // sendAccountDeletedEmail: this.accountOptions.sendAccountDeletedEmail,
+        // sendWelcomeEmail: this.accountOptions.sendWelcomeEmail,
+        // verifyProviderEmail: this.accountOptions.verifyProviderEmail,
       }
     });
-    // this.router.navigate(['/redux-readonly']);
+    this.router.navigate(['/redux-readonly']);
   };
 }
